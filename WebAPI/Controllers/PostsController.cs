56ddf91f05d3a9bafe7e5ff2e4172ae4a0eba174
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-10-11 14:02:15 +0200

    asd

---
+++ WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+ApiController]
+[Route("[controller]")]
+public class PostsController : ControllerBase
+{
+     private readonly IPostLogic postLogic;
+
+    public PostsController(IPostLogic postLogic)
+    {
+        this.postLogic = postLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Post>> CreateAsync(PostCreationDto dto)
+    {
+        try
+        {
+            Post created = await postLogic.CreateAsync(dto);
+            return Created($"/posts/{created.Id}", created);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Post>>> GetAsync([FromQuery] string? userName, [FromQuery] int? userId,
+        [FromQuery] bool? completedStatus, [FromQuery] string? titleContains)
+    {
+        try
+        {
+            SearchPostParametersDto parameters = new(userName, userId, completedStatus, titleContains);
+            var posts = await postLogic.GetAsync(parameters);
+            return Ok(todos);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult> UpdateAsync([FromBody] TodoUpdateDto dto)
+    {
+        try
+        {
+            await todoLogic.UpdateAsync(dto);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteAsync([FromRoute] int id)
+    {
+        try
+        {
+            await todoLogic.DeleteAsync(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<TodoBasicDto>> GetById([FromRoute] int id)
+    {
+        try
+        {
+            TodoBasicDto result = await todoLogic.GetByIdAsync(id);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
