f85d10a399b5804b9118d715ce3e113d9b75f24f
Reblayzer <104923443+Reblayzer@users.noreply.github.com>
2023-10-11 13:46:23 +0200

    Application

---
+++ Application/DaoInterfaces/IPostDao.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DaoInterfaces;
+
+public interface IPostDao
+{
+    Task<Post> CreateAsync(Post post);
+    Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto searchParameters);
+    Task UpdateAsync(Post todo);
+    Task<Post?> GetByIdAsync(int postId);
+    
+    Task DeleteAsync(int id);
+}
